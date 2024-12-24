4fbc337ca87b0572064d61779c9d155c4e8e8a8c
andrewsudardjo <sudardjoandrew123@gmail.com>
2024-12-24 15:27:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-// Use require for Electron since it's CommonJS
-const { app, BrowserWindow } = require('electron');
-const { createServer } = require('./app.js'); // Assuming your app.js exports createServer function
+import { app, BrowserWindow } from "electron";
+import { createServer } from './app.js';
 
 let mainWindow = null;
 
-function main() {
+function main(){
     const { server, port } = createServer(); 
 
     mainWindow = new BrowserWindow({
         webPreferences: {
-            nodeIntegration: true,  // Make sure this is enabled for your app to work as expected
+            nodeIntegration:true,
         }
     });
-    mainWindow.loadURL(`http://localhost:3000/`); // Adjust the URL if necessary
-    mainWindow.on('close', (event) => {
+    mainWindow.loadURL(`http://localhost:3000/`);
+    mainWindow.on('close', event => {
         mainWindow = null;
-    });
+    })
 };
 
 app.on('ready', main);
+
